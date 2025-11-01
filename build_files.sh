7c6415a0132836bdefed1ded8814d3feb3753dc9
Shahbaz Alam <sbzalam2024@gmail.com>
2025-11-01 17:02:08 +0530

    fixed build_fish.sh

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,7 @@
 
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+pip install -r requirements.txt
+
+python3.9 office_emp_proj/manage.py collectstatic --noinput
 echo "Build finished."
