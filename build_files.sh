8187cd59b6a555ca0b20e8b2b9b40f5f350cef99
Shahbaz Alam <sbzalam2024@gmail.com>
2025-11-01 17:12:07 +0530

    fixed vercel.json and build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,5 @@
 
 # Build the project
 echo "Building the project..."
-pip install -r requirements.txt
-
-python3.9 office_emp_proj/manage.py collectstatic --noinput
+echo "Static files build step..."
 echo "Build finished."
